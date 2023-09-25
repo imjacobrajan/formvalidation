0a16c461191c095c9662d6a5a571849d32b2b2cf
Jacob Samuel <imjacobsamuel@gmail.com>
2023-09-25 23:33:15 +0530

    added error message and green tick

--- js/script.js
+++ js/script.js
@@ -8,23 +8,34 @@ function submithandler() {
   var password = document.getElementById("password").value;
 
   if (name) {
-    document.getElementById("name").style = "border-bottom: 2px solid #04ed8d";
+    // document.getElementById("name").style = "border-bottom: 2px solid #04ed8d";
+    document.getElementById("name_err").style.visibility = "hidden";
+    document.getElementById("name_ok").style.visibility = "visible";
   } else {
-    document.getElementById("name").style = "border-bottom: 2px solid #ff5757";
-    document.getElementById("name").style.placeholderColor = "#ff5757";
+    // document.getElementById("name").style = "border-bottom: 2px solid #ff5757";
+    document.getElementById("name_err").style.visibility = "visible";
+    document.getElementById("name_ok").style.visibility = "hidden";
   }
 
   if (email) {
-    document.getElementById("email").style = "border-bottom: 2px solid #04ed8d";
+    // document.getElementById("email").style = "border-bottom: 2px solid #04ed8d";
+    document.getElementById("email_err").style.visibility = "hidden";
+    document.getElementById("email_ok").style.visibility = "visible";
   } else {
-    document.getElementById("email").style = "border-bottom: 2px solid #ff5757";
+    // document.getElementById("email").style = "border-bottom: 2px solid #ff5757";
+    document.getElementById("email_err").style.visibility = "visible";
+    document.getElementById("email_ok").style.visibility = "hidden";
   }
 
   if (password) {
-    document.getElementById("password").style =
-      "border-bottom: 2px solid #04ed8d";
+    // document.getElementById("password").style =
+    //   "border-bottom: 2px solid #04ed8d";
+    document.getElementById("password_err").style.visibility = "hidden";
+    document.getElementById("password_ok").style.visibility = "visible";
   } else {
-    document.getElementById("password").style =
-      "border-bottom: 2px solid #ff5757";
+    // document.getElementById("password").style =
+    // "border-bottom: 2px solid #ff5757";
+    document.getElementById("password_err").style.visibility = "visible";
+    document.getElementById("password_ok").style.visibility = "hidden";
   }
 }
